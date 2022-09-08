4d732b6423c3ab31a938f4881efa9a2e8e55c913
RodrigoGama <rodrigo1902.rb@gmail.com>
2022-09-07 21:14:56 -0300

    Version update

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "name": "Principled Tools",
     "description": "A collection of tools that will help you speed up your Principled Shader pipeline",
     "author": "Rodrigo Gama",
-    "version": (0, 5, 0),
+    "version": (0, 5, 1),
     "blender": (3, 3, 0),
     "location": "View3D",
     "category": "3D View"
